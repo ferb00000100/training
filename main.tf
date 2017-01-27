3a4e0a75b53c99c13df2eaad4190363ae98f2905
HashiCorp Training <training@hashicorp.com>
2017-01-27 17:56:02 +0000

    bump

---
+++ main.tf
@@ -0,0 +1,57 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your subnet ID is:
+#
+#     subnet-ddd57685
+#
+# Your security group ID is:
+#
+#     sg-29ef374e
+#
+# Your AMI ID is:
+#
+#     ami-30217250
+#
+# Your Identity is:
+#
+#     autodesk-buffalo
+#
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "us-west-1"
+}
+
+variable "num_webs" {
+  default = "2"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  # count                  = "2"
+  count                  = "${var.num_webs}"
+  instance_type          = "t2.micro"
+  ami                    = "ami-30217250"
+  subnet_id              = "subnet-ddd57685"
+  vpc_security_group_ids = ["sg-29ef374e"]
+
+  tags {
+    Identity = "autodesk-buffalos"
+    Name     = "MyWeb Server"
+    Service  = "webapp"
+    MyName   = "web ${count.index+1}/${var.num_webs}"
+  }
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
